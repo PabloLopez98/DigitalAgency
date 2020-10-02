b31d20adb1a5a589766678c22cea83e52c526927
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-10-02 02:52:27 -0700

    focus on menu nav later

--- gsapanimations.js
+++ gsapanimations.js
@@ -3,3 +3,14 @@ tl.from("#nav", { y: -200, opacity: 0, duration: 1 });
 tl.from("#title", { y: 200, opacity: 0, duration: 1.5 });
 tl.from("#description", { y: 200, opacity: 0, duration: 1 });
 tl.from("#side-text", { opacity: 0, duration: 1 });
+
+gsap.to("#img-container", {
+    scrollTrigger: {
+        trigger: "#img-container",
+        start: "center center",
+        end: "bottom top",
+        scrub: true,
+        //markers: true
+    },
+    height: "100vh"
+})
