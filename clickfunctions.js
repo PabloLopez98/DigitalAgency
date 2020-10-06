21671fcdb3e8e0131a8afbcd14fcd7f1137a89d5
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-10-06 16:22:54 -0700

    Fix smooth scrolling

--- clickfunctions.js
+++ clickfunctions.js
@@ -3,22 +3,22 @@ let thenav = document.getElementById('nav')
 thehamburgericon.addEventListener('click', () => {
     if (thenav.style.color === 'pink') {
         var t1 = gsap.timeline()
-        t1.to("#left", { width: '0vw', duration: .3 });
-        t1.to("#right", { width: '0vw', duration: .3 });
-        t1.to("#left", { height: '0vh', duration: .3 });
-        t1.to("#right", { height: '0vh', duration: .3 });
-        t1.to("#menu", { height: '0vh', duration: .3 });
-        t1.to("#menu", { width: '50vw', duration: .3 });
+        t1.to("#right", { opacity: 0, duration: .5 });
+        t1.to("#left", { opacity: 0, duration: .5 });
+        t1.to("#left", { visibility: 'visible', duration: 0 });
+        t1.to("#right", { visibility: 'visible', duration: .1 });
+        t1.to("#menu", { width: '50vh', duration: .3 });
+        t1.to("#menu", { height: '0vw', duration: .3 });
         thenav.style.color = 'white'
         thehamburgericon.className = 'fas fa-grip-lines'
     } else {
         var t2 = gsap.timeline()
         t2.to("#menu", { height: '100vh', duration: .3 });
         t2.to("#menu", { width: '100vw', duration: .3 });
-        t2.to("#left", { height: '100vh', duration: .3 });
-        t2.to("#right", { height: '100vh', duration: .3 });
-        t2.to("#left", { width: '50vw', duration: .3 });
-        t2.to("#right", { width: '50vw', duration: .3 });
+        t2.to("#left", { visibility: 'visible', duration: 0 });
+        t2.to("#right", { visibility: 'visible', duration: .1 });
+        t2.to("#left", { opacity: 1, duration: .5 });
+        t2.to("#right", { opacity: 1, duration: .5 });
         thenav.style.color = 'pink'
         thehamburgericon.className = 'fas fa-times'
     }
